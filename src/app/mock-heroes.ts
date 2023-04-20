235baf1937bdc56c2b693c6a4072a4f21716e475
kaung <kaungmyathun7@gmail.com>
2023-04-20 14:12:31 +0630

    learn: new angular

---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,9 @@
+import {Hero} from './Hero'
+
+export const HEROES: Hero[]=[
+  {id:5,name:"Dr Doom"},
+  {id:6,name:"John Wick"},
+  {id:7,name:"Stella"},
+  {id:8,name:"Tony"},
+  {id:9,name:"Thor"}
+]
